a95c11aa4515f00c0e8464db3b3f56e133034e1a
Vikash Gupta <mrvikash700@gmail.com>
2026-09-18 18:28:47 +0530

    member route added

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,10 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       resources :categories
-      resources :products
-      get "/product_detail/:id", to: 'products#product_detail'
+      resources :products do
+        get :product_detail, on: :member  # get "/product_detail/:id", to: 'products#product_detail' both will work fine
+      end
+
 
       resources :product_variants
 
